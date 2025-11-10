49d730e3341d1bfc0829b6929f8561b5316b314f
jeremy !! <f.tzgeraldz@gmail.com>
2025-11-10 17:30:26 -0500

    Created basic footer and nav

---
+++ src/lib/footer/footer.svelte
@@ -0,0 +1,10 @@
+<script>
+  import "./footer.css";
+</script>
+
+<footer>
+  <div>
+    <a href="/">Own an Organization? Log in here!</a>
+    <p>Copyright 2025 All Rights Reserved</p>
+  </div>
+</footer>
--- src/lib/nav/nav.svelte
+++ src/lib/nav/nav.svelte
@@ -1,6 +1,16 @@
+<script>
+  import "./nav.css";
+</script>
+
 <nav>
-  <a href="/">Home</a>
-  <a href="/events">Events</a>
-  <a href="/orgs">Organizations</a>
-  <a href="/profile">My Profile</a>
+  <div class="logo">
+    <!-- <a href="/"><img src="/logo.png" alt="EventSea Logo" /></a> -->
+    Logo
+  </div>
+  <div class="links">
+    <a href="/">Home</a>
+    <a href="/events">Events</a>
+    <a href="/orgs">Organizations</a>
+    <a href="/profile">My Profile</a>
+  </div>
 </nav>
