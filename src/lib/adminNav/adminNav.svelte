80ea6529422338d4d50b68d15aa301f80621084f
jeremy !! <jereblac@iu.edu>
2025-12-06 02:12:04 -0500

    Added small CSS adjustments, built "Saved _" pages

--- src/lib/adminNav/adminNav.svelte
+++ src/lib/adminNav/adminNav.svelte
@@ -1,44 +1,52 @@
 <script>
-    import './adminNav.css';
-    import 'iconify-icon';
-    
+  import "./adminNav.css";
+  import Icon from "@iconify/svelte";
 </script>
 
 <div class="sidebarNav">
-    <section class="orgPfp">
-        <div class="infoLine">
-            <img src="https://picsum.photos/50" alt="pfp Placeholder">
-            <p>[ORG NAME]</p>
-        </div>
-        <p>Org Since: [YEAR]</p>
-        <p>Members: [MEMBER COUNT]</p>
-    </section>
-    <section class="buttons">
-        <ul>
-            <li>
-                <iconify-icon icon="fa7-solid:house-chimney" width="30px" height="30px"></iconify-icon>
-                <a href="/adminHome">Home</a>
-            </li>
-            <li>
-                <iconify-icon icon="mdi:calendar" width="30px" height="30px"></iconify-icon>
-                <a href="/adminEventCalendar">Event Calendar</a>
-            </li>
-            <li>
-                <iconify-icon icon="mdi:event-add" width="30" height="30"></iconify-icon>
-                <a href="/adminNewEvent">New Event</a>
-            </li>
-            <li>
-                <iconify-icon icon="streamline:announcement-megaphone-remix" width="30" height="30"></iconify-icon>
-                <a href="/adminNewAnnouncement">New Announcement</a>
-            </li>
-            <li>
-                <iconify-icon icon="majesticons:users-line" width="30" height="30"></iconify-icon>
-                <a href="/adminMemberList">Member List</a>
-            </li>
-            <li class="logoutButton" id="adminLogout">
-                <iconify-icon icon="ic:round-exit-to-app" width="30" height="30"></iconify-icon>
-                <span>Log Out</span>
-            </li>
-        </ul>
-    </section>
+  <section class="orgPfp">
+    <div class="infoLine">
+      <img src="https://picsum.photos/50" alt="pfp Placeholder" />
+      <p>[ORG NAME]</p>
+    </div>
+    <p>Org Since: [YEAR]</p>
+    <p>Members: [MEMBER COUNT]</p>
+  </section>
+  <section class="buttons">
+    <ul>
+      <li>
+        <iconify-icon icon="fa7-solid:house-chimney" width="30px" height="30px"
+        ></iconify-icon>
+        <a href="/adminHome">Home</a>
+      </li>
+      <li>
+        <iconify-icon icon="mdi:calendar" width="30px" height="30px"
+        ></iconify-icon>
+        <a href="/adminEventCalendar">Event Calendar</a>
+      </li>
+      <li>
+        <iconify-icon icon="mdi:event-add" width="30" height="30"
+        ></iconify-icon>
+        <a href="/adminNewEvent">New Event</a>
+      </li>
+      <li>
+        <iconify-icon
+          icon="streamline:announcement-megaphone-remix"
+          width="30"
+          height="30"
+        ></iconify-icon>
+        <a href="/adminNewAnnouncement">New Announcement</a>
+      </li>
+      <li>
+        <iconify-icon icon="majesticons:users-line" width="30" height="30"
+        ></iconify-icon>
+        <a href="/adminMemberList">Member List</a>
+      </li>
+      <li class="logoutButton" id="adminLogout">
+        <iconify-icon icon="ic:round-exit-to-app" width="30" height="30"
+        ></iconify-icon>
+        <span>Log Out</span>
+      </li>
+    </ul>
+  </section>
 </div>
