d8e2fa377f854d6d5bdd49db317b5f51b074bbc3
Ev Schmitt <evanjschmitt@gmail.com>
2025-12-08 15:23:27 -0500

    Vercel Fixes (Hopefully)

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        // ... (your paths config) ...
+        
         adapter: adapter()
     }
 };
