baa472f4c110ef280895ed9ba6f71246b53f877d
Ev Schmitt <evanjschmitt@gmail.com>
2025-12-07 19:06:27 -0500

    Hotfix 01

---
+++ src/lib/event/event.svelte
@@ -0,0 +1,69 @@
+<script>
+  import "./event.css";
+  import Icon from "@iconify/svelte";
+  export let img = "/placeholder.jpg";
+  export let alt = "Alt text for the collection image.";
+  export let name = "Default Event Name";
+  export let description = "Default description goes here.";
+  export let attendees = 0;
+  export let date = "Date";
+  export let time = "Time";
+  export let location = "Location";
+  export let org = "Organization Name";
+</script>
+
+<div class="eventCard">
+  <img src={img} {alt} class="eventImage" />
+  <div class="eventText">
+    <h3>{name}</h3>
+    <p>{description}</p>
+    <span
+      ><Icon
+        icon="ion:people"
+        class="margin"
+        width="16"
+        height="16"
+      />{attendees} people going</span
+    >
+    <br />
+    <span
+      ><Icon
+        icon="uiw:date"
+        class="margin"
+        width="16"
+        height="16"
+      />{date}</span
+    > <br />
+    <span
+      ><Icon
+        icon="tabler:clock"
+        class="margin"
+        width="16"
+        height="16"
+      />{time}</span
+    >
+    <br />
+    <span
+      ><Icon
+        icon="mingcute:location-fill"
+        class="margin"
+        width="16"
+        height="16"
+      />{location}</span
+    > <br />
+    <span
+      ><a href="/"
+        ><Icon
+          icon="octicon:organization-16"
+          class="margin"
+          width="16"
+          height="16"
+        />{org}</a
+      ></span
+    >
+    <div class="eventButtons">
+      <button><a href="/savedEvents">Add to My Events</a></button>
+      <button><a href="/singleEvent">Learn More</a></button>
+    </div>
+  </div>
+</div>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,7 +3,7 @@ export { default as Nav } from "./nav/nav.svelte";
 export { default as Footer } from "./footer/footer.svelte";
 export { default as AdminNav} from "./adminNav/adminNav.svelte"
 export { default as AdminFooter} from "./adminFooter/adminFooter.svelte"
-export { default as Event } from "../../oldAssets/event/event.svelte";
+export { default as Event } from "./event/event.svelte";
 export { default as EventCard } from "./eventCard/eventCard.svelte";
 export { default as Org } from "./organization/organization.svelte";
 export { default as EventForm } from "./eventForm/eventForm.svelte";
