d2619fc6bd51d624214e29a957936609cad5c10e
Ev Schmitt <evanjschmitt@gmail.com>
2025-12-08 15:16:28 -0500

    Svelte Config Change

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,11 @@
-// import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// svelte.config.js
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-static';
-
 const config = {
     kit: {
-        paths: { base: '/~ejschmit/public_html/eventSea' },
-        adapter: adapter({
-            // Set the output directory name
-            pages: 'build', 
-            assets: 'build',
-            fallback: 'index.html' // Required for single-page application (SPA) mode
-        })
+        // ... (your paths config) ...
+        adapter: adapter()
     }
 };
 
